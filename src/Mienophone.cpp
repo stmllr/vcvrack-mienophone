c66d2bc82c0f53a34bb43312941f5d42333277be
Steffen Müller <github2@xonx.de>
2020-08-04 02:09:45 +0200

    Fix module

--- src/Mienophone.cpp
+++ src/Mienophone.cpp
@@ -59,14 +59,14 @@ struct MienophoneWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 54.135)), module, Mienophone::ANGER_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 94.325)), module, Mienophone::CONTEMPT_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 134.515)), module, Mienophone::DISGUST_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 174.755)), module, Mienophone::FEAR_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 214.895)), module, Mienophone::HAPPYNESS_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 255.085)), module, Mienophone::NEUTRAL_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 295.26)), module, Mienophone::SADNESS_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(21.71, 335.465)), module, Mienophone::SURPRISE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, -49.239)), module, Mienophone::ANGER_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 4.348)), module, Mienophone::CONTEMPT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 57.935)), module, Mienophone::DISGUST_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 111.589)), module, Mienophone::FEAR_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 165.109)), module, Mienophone::HAPPYNESS_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 218.696)), module, Mienophone::NEUTRAL_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 272.263)), module, Mienophone::SADNESS_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.194, 325.87)), module, Mienophone::SURPRISE_OUTPUT));
 	}
 };
 
